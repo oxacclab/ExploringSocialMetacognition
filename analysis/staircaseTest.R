252dfa216b07f8a5dba5d5f1af2b591989fba3d3
mjaquiery <m.jaquiery@sussex.ac.uk>
2018-09-14 17:18:45 +0100

    More practice
    * added additional practice trials to Dot Task
    * staircase exploration in analysis scripts

--- analysis/staircaseTest.R
+++ analysis/staircaseTest.R
@@ -1,11 +1,32 @@
 # Staircase procedure debugging
+library(ggplot2)
+graph <- function(df) {
+  ggplot(df, aes(x=gen, y=pCorrect, colour=id)) + 
+    geom_line(alpha = 0.25) + 
+    stat_summary(geom = 'line', fun.y = mean, colour = 'red') +
+    stat_summary(geom = 'ribbon', fun.data = mean_cl_normal, colour = NA, fill = 'red', alpha = 0.1) +
+    scale_y_continuous(limits=c(0,1),breaks=seq(0,1,.05)) +
+    stat_summary(geom = 'ribbon', fun.data = mean_cl_normal, colour = NA, fill = 'red', alpha = 0.05) +
+    annotate(geom = 'text', label = c('Mean', 'CL95% Low', 'CL95% High'),
+             y = c(.175,.075,.025), x = 230, hjust = 1) +
+    annotate(geom = 'text', 
+             label = paste(round(mean_cl_normal(df$pCorrect[df$gen==max(df$gen)]),3)),
+             y = c(.175,.075,.025), x = 240) +
+    theme_light() +
+    theme(panel.grid.minor = element_blank(),
+          panel.grid.major.x = element_blank(),
+          legend.position = 'top') + 
+    theme(legend.position = 'none') +
+    labs(title = 'Cumulative accuracy plot for simulated agents')
+}
+
 
 ## Simulation ####
 # first answer is always correct
 # 10,000 agents
-nAgents <- 1000
+nAgents <- 20
 pCorrect <- rep(1, nAgents) 
-agents <- data.frame(id = 1:nAgents, gen = rep(0, nAgents), pCorrect, correct)
+agents <- data.frame(id = as.factor(1:nAgents), gen = rep(0, nAgents), pCorrect, correct)
 # 250 trials
 for(i in 0:250) {
   mask <- agents$gen == i
@@ -18,8 +39,10 @@ for(i in 0:250) {
     newAgents$pCorrect[!newAgents$correct] <- newAgents$pCorrect[!newAgents$correct] + .01
     # 2 hits - make harder
     newAgents$pCorrect[agents$correct[mask] 
-                       & agents$correct[agents$gen==(i-1)]] <- newAgents$pCorrect[agents$correct[mask] 
-                                                                                  & agents$correct[agents$gen==(i-1)]] - .01
+                       & agents$correct[agents$gen==(i-1)]
+                       & agents$pCorrect[mask] == agents$pCorrect[agents$gen==(i-1)]] <- newAgents$pCorrect[agents$correct[mask] 
+                                                                                  & agents$correct[agents$gen==(i-1)]
+                                                                                  & agents$pCorrect[mask] == agents$pCorrect[agents$gen==(i-1)]] - .01
     newAgents$pCorrect[newAgents$pCorrect < 0.00] <- 0.00
     newAgents$pCorrect[newAgents$pCorrect > 1.00] <- 1.00
   }
@@ -27,17 +50,19 @@ for(i in 0:250) {
 }
 
 ggplot2::mean_cl_normal(agents$pCorrect[agents$gen==max(agents$gen)])
+graph(agents)
 
 ## Simulation 2 - sigmoid agents ####
 sigmoid <- function(s) 1 / (1 + exp(-s))
 
-nAgents <- 1000
+nAgents <- 20
 difficulty <- rep(-1,nAgents)
-agents <- data.frame(id=1:nAgents, gen=rep(0,nAgents), difficulty)
+agents <- data.frame(id=as.factor(1:nAgents), gen=rep(0,nAgents), difficulty)
 
 for(i in 0:250) {
   mask <- agents$gen == i
   agents$correct[mask] <- runif(length(mask)) > sigmoid(agents$difficulty[mask])
+  agents$pCorrect[mask] <- sapply(agents$id[mask], function(id) mean(agents$correct[agents$id==id]))
   newAgents <- agents[mask, ]
   newAgents$gen <- i+1
   if(i > 0) {
@@ -48,21 +73,23 @@ for(i in 0:250) {
     newAgents$difficulty[agents$correct[mask] 
                          & agents$correct[agents$gen==(i-1)]] <- 
       newAgents$difficulty[agents$correct[mask]
-                           & agents$correct[agents$gen==(i-1)]] + .11
+                           & agents$correct[agents$gen==(i-1)]] + .1
   }
   agents <- rbind(agents, newAgents)
 }
 ggplot2::mean_cl_normal(aggregate(correct ~ id, agents, mean)$correct)
+graph(agents)
 
 
 # Simulation 3 - Sigmoid + discrete steps
-nAgents <- 1000
+nAgents <- 20
 difficulty <- rep(-1,nAgents)
-agents <- data.frame(id=1:nAgents, gen=rep(0,nAgents), difficulty)
+agents <- data.frame(id=as.factor(1:nAgents), gen=rep(0,nAgents), difficulty)
 
 for(i in 0:250) {
   mask <- agents$gen == i
   agents$correct[mask] <- runif(length(mask)) > sigmoid(agents$difficulty[mask])
+  agents$pCorrect[mask] <- sapply(agents$id[mask], function(id) mean(agents$correct[agents$id==id]))
   newAgents <- agents[mask, ]
   newAgents$gen <- i+1
   if(i > 0) {
@@ -75,8 +102,12 @@ for(i in 0:250) {
                          & agents$difficulty[mask] == agents$difficult[agents$gen==(i-1)]] <- 
       newAgents$difficulty[agents$correct[mask]
                            & agents$correct[agents$gen==(i-1)]
-                           & agents$difficulty[mask] == agents$difficult[agents$gen==(i-1)]] + .11
+                           & agents$difficulty[mask] == agents$difficult[agents$gen==(i-1)]] + .1
   }
   agents <- rbind(agents, newAgents)
 }
 ggplot2::mean_cl_normal(aggregate(correct ~ id, agents, mean)$correct)
+graph(agents)
+
+
+#Learning effects?
