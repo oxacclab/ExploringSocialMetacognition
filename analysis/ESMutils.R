eaf4757165592b66050f9a807e90d70dae3f6acc
mjaquiery <m.jaquiery@sussex.ac.uk>
2018-11-09 12:36:22 +0000

    Basic descriptives
    * Added nicer descriptive statistic summaries
    * Streamlined the RMd files slightly using external scripts

---
+++ analysis/ESMutils.R
@@ -0,0 +1,181 @@
+# Exploring Social Metacognition support functions and variables ----------
+
+
+# Libraries ---------------------------------------------------------------
+# Load the dependencies for the main scripts
+
+# Parsing JSON files
+if(!require(jsonlite)) {
+  install.packages("jsonlite")
+  library(jsonlite)
+}
+# Calculating Bayes factors
+if(!require(BayesFactor)) {
+  install.packages('BayesFactor')
+  library(BayesFactor)
+}
+# Plots
+if(!require(tidyverse)) {
+  install.packages('tidyverse')
+  library(tidyverse)
+}
+# Long-to-wide conversions
+if(!require(reshape2)) {
+  install.packages('reshape2')
+  library(reshape2)
+}
+# Linear modelling
+if(!require(lme4)) {
+  install.packages('lme4')
+  library(lme4)
+}
+# CohensD calculations
+if(!require(lsr)) {
+  install.packages('lsr')
+  library(lsr)
+}
+# RMarkdown to HTML conversion
+if(!require(knitr)) {
+  install.packages('knitr')
+  library(knitr)
+}
+# Confidence intervals?
+if(!require(Hmisc)) {
+  install.packages('Hmisc')
+  library(Hmisc)
+}
+# Brier scores
+if(!require(scoring)) {
+  install.packages('scoring')
+  library(scoring)
+}
+
+# Reference and mapping functions -----------------------------------------
+
+# Return the advice type of an advisor for participant with row number=pid
+getAdviceTypeById <- function(aid, pid, advisor.data.frame) {
+  type <- advisor.data.frame[which(advisor.data.frame$participantId==pid),]
+  type <- type[which(type$id==aid),]
+  if (length(type) > 0)
+    return(type$adviceType)
+  return(NA)
+}
+# Return a vector of advice types for trial list t
+getAdviceType <- function (t, participant.data.frame, advisor.data.frame, forceRecalculate = FALSE) {
+  # shortcut if we already calculated this
+  if('adviceType' %in% colnames(t) && !forceRecalculate)
+    return(t$adviceType)
+  out <- vector(length=dim(t)[1])
+  for (i in seq(length(out))) {
+    if (t$advisorId[i]==0) {
+      # no advisor
+      out[i] <- NA;
+    } else {
+      pid <- t$participantId[i]
+      out[i] <- getAdviceTypeById(t$advisorId[i], pid, advisor.data.frame)
+    }
+  }
+  return(out)
+}
+
+#' Find the confidence shift in a given trial
+#' @param t trial list
+#' @param rawShift whether to report the confidence shift without adjusting for the assymetric scale
+#' @param forceRecalulate if true, simply return the appropriate column from t if it exists already
+#' @return a vector of confidence shifts for trial list t
+getConfidenceShift <- function (t, rawShift = FALSE, forceRecalculate = FALSE) {
+  scaleMaximum <- 50
+  # shortcut if we already calculated this
+  if('confidenceShift' %in% colnames(t) && !forceRecalculate)
+    return(t$confidenceShift)
+  out <- vector(length=dim(t)[1])
+  for (i in seq(length(out))) {
+    if (is.na(t$finalConfidence[i])) { # no advisor
+      out[i] <- NA
+    } else {
+      max.shift <- scaleMaximum - t$initialConfidence[i]
+      if(t$initialAnswer[i]==t$finalAnswer[i])
+        out[i] <- t$finalConfidence[i]-t$initialConfidence[i] # same side
+      else
+        out[i] <- -1 * (t$finalConfidence[i]+t$initialConfidence[i]) # switched sliders, so went to 0 on the first one
+      out[i] <- ifelse((abs(out[i]) > max.shift) & rawShift == F, max.shift*sign(out[i]), out[i])
+    }
+  }
+  return(out)
+}
+
+# Return a vector of influence for trial list t
+#' @param t trial list
+#' @param rawShift whether to report the influence without adjusting for the assymetric scale
+#' @param forceRecalulate if true, simply return the appropriate column from t if it exists already
+#' @return a vector of influence for trial list t
+getInfluence <- function (t, rawShift = FALSE, forceRecalculate = FALSE) {
+  # shortcut if we already calculated this
+  if('influence' %in% colnames(t) && !forceRecalculate)
+    return(t$influence)
+  out <- vector(length=dim(t)[1])
+  for (i in seq(length(out))) {
+    if (t$advisorId[i] == 0) { # no advisor
+      out[i] <- NA
+    } else {
+      if (t$advisorAgrees[i])
+        out[i] <- getConfidenceShift(t[i,], rawShift, forceRecalculate) # amount confidence increased
+      else
+        out[i] <- -1 * getConfidenceShift(t[i,], rawShift, forceRecalculate) # -1 * amount confidence increased
+    }
+  }
+  return(out)
+}
+
+#' Get the name of the advice type
+#' @param adviceType the advice type to fetch the name for
+#' @param long whether to return the long name
+#' @return string of the advice type, or NA by default
+getAdviceTypeName <- function(adviceType, long = FALSE) {
+  if(length(adviceType)>1) {
+    out <- NULL
+    for(aT in adviceType)
+      out <- c(out, getAdviceTypeName(aT))
+    return(out)
+  }
+  if(adviceType==adviceTypes$neutral)
+    return(ifelse(long, 'neutral', 'Ntl'))
+  if(adviceType==adviceTypes$AiC)
+    return(ifelse(long,'Agree-in-confidence', 'AiC'))
+  if(adviceType==adviceTypes$AiU)
+    return(ifelse(long,'Agree-in-uncertainty', 'AiU'))
+  if(adviceType==adviceTypes$HighAcc)
+    return(ifelse(long,'High accuracy', 'HighAcc'))
+  if(adviceType==adviceTypes$LowAcc)
+    return(ifelse(long,'Low accuracy', 'LowAcc'))
+  if(adviceType==adviceTypes$HighAgr)
+    return(ifelse(long, 'High agreement', 'HighAgr'))
+  if(adviceType==adviceTypes$LowAgr)
+    return(ifelse(long, 'Low agreement', 'LowAgr'))
+  return(ifelse(long, 'None', NA))
+}
+
+
+# Global variables --------------------------------------------------------
+
+# advice types: neutral, agree-in-confidence, and agree-in-uncertainty
+adviceTypes <- list(neutral=0, 
+                    AiC=3, AiU=4, 
+                    HighAcc=5, LowAcc=6,
+                    HighAgr=7, LowAgr=8)
+trialTypes <- list(catch=0, force=1, choice=2)
+confidenceCategories <- list(low=0, medium=1, high=2)
+
+# Advisor questionnaire dimensions
+questionnaireDimensions <- list(accurate=1,
+                                like=2,
+                                trust=3,
+                                influence=4)
+
+# The Advisor portraits have properties which might affect ratings, so we should investigate these:
+portraitDetails <- data.frame(
+  portraitId = 1:5,
+  category = factor(c('w', 'b', 'w', 'b', 'w')),
+  blackProp = c(0, .99, 0, .99, .01),
+  age = c(28.7, 24.9, 23.3, 24.6, 23.7)
+)
---
+++ analysis/miscFunctions.R
@@ -0,0 +1,105 @@
+# General utility functions -----------------------------------------------
+
+# Presentation functions --------------------------------------------------
+
+#' Format proportion to be a string beginning with a decimal point.
+#' Does not respect scientific notation options
+#' @param proportion number to convert to string
+#' @param precision decimal places to preserve
+#' @return \{code}proportion stripped of leading 0s and rounded to \{code}precision decimal places
+prop2str <- function(proportion, precision = 2) {
+  if(length(proportion) > 1)
+    return(sapply(proportion, prop2str))
+  if(is.nan(proportion))
+    return(as.character(proportion))
+  proportion <- round(proportion, precision)
+  if(abs(proportion) < 1)
+    x <- sub('^-?0\\.', ifelse(proportion < 0, '-.', '.'), as.character(proportion))
+  else 
+    x <- as.character(proportion)
+  dot <- regexpr('.', x, fixed = T)
+  if(dot == -1) {
+    x <- paste0(x,'.')
+    dot <- regexpr('.', x, fixed = T)
+  }
+  right <- substr(x, dot, dot+precision) # portion of x after 0
+  right <- paste0(right, strrep('0',precision-nchar(right)+1))
+  x <- paste0(substr(x, 1, dot-1), right)
+  return(x)
+}
+
+#' Print the results of a t-test as we would like to see them reported in a paper
+#' @param results t.test result to print
+#' @param d cohensD of the t.test
+#' @param doPrint whether to call print() on the output
+#' @return string with neatly printed mean
+prettyPrint <- function(results, d = NULL, doPrint = T) {
+  es <- NULL
+  if(!is.null(d))
+    es <- paste0(' , d = ', round(d,2))
+  out <- paste0('t(',results$parameter,') = ',round(results$statistic,2),
+                ' [',round(attr(results$conf.int, "conf.level")*100),'%CI: ',
+                round(results$conf.int[[1]],2), ', ', round(results$conf.int[[2]],2),'],',
+                ' p = ',prop2str(results$p.value,3), es)
+  if(doPrint)
+    print(out)
+  return(out)
+}
+
+#' Print the mean and CIs of a vector
+#' @param vector data in
+#' @param label prefix for the stats
+#' @param doPrint whether to call print() on the output
+#' @param conf.int width of the confidence intervals
+#' @param na.rm whether NA values are removed before averaging
+#' @param decimals decimal places to round to
+#' @return string representation of the mean, CIs, and range of the \{code}vector
+printMean <- function(vector, label = 'Mean', doPrint = T, conf.int = .95, na.rm = F, decimals = 2) {
+  mu <- mean(vector, na.rm = na.rm)
+  s <- sd(vector, na.rm = na.rm)
+  n <- length(vector)
+  error <- qnorm(1-(1-conf.int)/2)*s/sqrt(n) # 95% confidence interval width
+  ci.low <- mu - error
+  ci.high <- mu + error
+  r <- round(range(vector, na.rm = na.rm), decimals)
+  out <- paste0(label,'=', round(mu,decimals), ' [', round(conf.int,decimals)*100, '%CI: ',
+               round(ci.low,decimals), ', ', round(ci.high,decimals),'] [Range: ',
+               r[[1]], ', ', r[[2]], ']')
+  if(doPrint)
+    print(out)
+  return(out)
+}
+
+#' Quickly get t-test and bayes factor, and means for two vectors
+#' @param v1 first vector
+#' @param v2 second vector
+#' @param label1 label of the first vector
+#' @param label2 label of the second vector
+#' @param paried whether the vectors are paired observations
+#' @param doPrint whether to print the results while running
+#' @return string with neat details of the comparison
+quickCompareVectors <- function(v1, v2, label1 = 'Vector 1', label2 = 'Vector2', paired = F, doPrint = T) {
+  result <- t.test(v1, v2, paired = paired)
+  d <- cohensD(v1, v2)
+  bf <- ttestBF(v1, v2, paired = paired)
+  out <- paste0(printMean(v1, label1, F), ' \n', printMean(v2, label2, F))
+  out <- paste0(out, ' \n', prettyPrint(result, d, doPrint = F), ', BF = ', round(exp(bf@bayesFactor$bf),3))
+  if(doPrint) {
+    printMean(v1, label1)
+    printMean(v2, label2)
+    print(paste0(prettyPrint(result, d, doPrint = F), ', BF = ', round(exp(bf@bayesFactor$bf),3)))
+  }
+  return(out)
+}
+
+# ggplot Presentation -----------------------------------------------------
+
+style <- theme_light() +
+  theme(panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_blank(),
+        legend.position = 'top')
+
+style.long <- style + theme(legend.position = 'none')
+
+# Utility functions -------------------------------------------------------
+
