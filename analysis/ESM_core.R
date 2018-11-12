ef04b4efbac9963500bd55a09ad524a9353fd2cc
mjaquiery <m.jaquiery@sussex.ac.uk>
2018-11-12 14:26:47 +0000

    ANOVA fix
    * fixed ANOVAs not using factor() factors and therefore giving spurious results
    * moved to a distributed filestructure to reduce redundency between the analysis .Rmd files
    * recreated the output files

---
+++ analysis/ESM_core.R
@@ -0,0 +1,250 @@
+
+# Prerequisites -----------------------------------------------------------
+
+source('ESM_utils.R') # libraries and some manipulation functions
+source('miscFunctions.R') # neat printing, etc
+
+# Variables ---------------------------------------------------------------
+
+dodge.width.agreements <- .8
+
+# Manipulation check functions ----------------------------------------------
+
+#' Aggregate trial agreement by confidence, advisor, and initial correctness
+#' @param trials trial set
+#' @param factorize whether to convert aggregate-by values to factors
+#' @param stripMissing whether to remove observations without complete data for each factor combination
+tmp.agreementContingencies <- function(trials, factorize = T, stripMissing = T) {
+  tmp <- aggregate(advisorAgrees ~ pid + confidenceCategory + adviceType + initialCorrect, data = trials, FUN = mean)
+  if(!factorize)
+    return(tmp)
+  for(n in names(tmp[-(length(tmp))]))
+    tmp[ ,n] <- factor(tmp[ ,n])
+  if(!stripMissing)
+    return(tmp)
+  # drop missing observations
+  miss <- aggregateMissing(tmp, 'pid', list('confidenceCategory', 'adviceType', 'initialCorrect'))
+  tmp <- tmp[!(tmp$pid %in% miss), ]
+}
+
+#' ANOVA of agreement by correcteness, confidence, and advisor
+aov.agreementContingencies <- function(trials) {
+  tmp <- tmp.agreementContingencies(trials)
+  aov(advisorAgrees ~ confidenceCategory * adviceType * initialCorrect + 
+        Error(pid / (confidenceCategory + adviceType + initialCorrect)), data = tmp)
+}
+
+#' Graph of agreement
+#' @inheritDotParams tmp.agreementContingencies
+#' Additional arguments are passed to \code{tmp.agreementContingencies}
+gg.agreementContingencies <- function(trials, ...) {
+  tmp <- tmp.agreementContingencies(trials, ...)
+  ggplot(tmp, aes(y = advisorAgrees, x = as.factor(confidenceCategory), colour = as.factor(adviceType))) +
+    stat_summary(geom = 'point', size = 3, fun.y = mean, position = position_dodge(dodge.width.agreements)) +
+    stat_summary(geom = 'errorbar', fun.data = mean_cl_boot, position = position_dodge(dodge.width.agreements), size = 0.2) +
+    geom_point(alpha = 0.5, position = position_dodge(dodge.width.agreements)) +
+    facet_wrap(~initialCorrect, labeller = label_both) +
+    scale_x_discrete(name = 'Confidence Category', labels = c('Low', 'Med', 'High')) +
+    scale_color_discrete(name = 'Advice Type', labels = getAdviceTypeName(unique(tmp$adviceType))) +
+    scale_y_continuous(name = 'Advisor Agreement') +
+    labs(title = 'Observed agreement rate on Forced trials') +
+    style
+}
+
+#' Aggregate advisor agreement by confidence, advisor, and trial type
+#' @param trials trial set
+#' @param factorize whether to convert aggregate-by values to factors
+#' @param stripMissing whether to remove observations without complete data for each factor combination
+tmp.agreementContingenciesByType <- function(trials, factorize = T, stripMissing = T) {
+  tmp <- aggregate(advisorAgrees ~ pid + confidenceCategory + adviceType + typeName, 
+                   data = trials[trials$initialCorrect==T, ], FUN = mean)
+  if(!factorize)
+    return(tmp)
+  for(n in names(tmp[-(length(tmp))]))
+    tmp[ ,n] <- factor(tmp[ ,n])
+  if(!stripMissing)
+    return(tmp)
+  # drop missing observations
+  miss <- aggregateMissing(tmp, 'pid', list('confidenceCategory', 'adviceType', 'typeName'))
+  tmp <- tmp[!(tmp$pid %in% miss), ]
+}
+
+#' ANOVA of agreement by trial type, confidence, and advisor
+aov.agreementContingenciesByType <- function(trials) {
+  tmp <- tmp.agreementContingenciesByType(trials)
+  aov(advisorAgrees ~ confidenceCategory * adviceType * typeName + 
+        Error(pid / (confidenceCategory + adviceType + typeName)), data = tmp)
+}
+
+#' Graph of agreement
+#' @inheritDotParams tmp.agreementContingenciesByType
+#' Additional arguments are passed to \code{tmp.agreementContingenciesByType}
+gg.agreementContingenciesByType <- function(trials, ...) {
+  tmp <- tmp.agreementContingenciesByType(trials, ...)
+  ggplot(tmp, aes(y = advisorAgrees, x = as.factor(confidenceCategory), colour = as.factor(adviceType))) +
+    stat_summary(geom = 'point', size = 3, fun.y = mean, position = position_dodge(dodge.width.agreements)) +
+    stat_summary(geom = 'errorbar', fun.data = mean_cl_boot, position = position_dodge(dodge.width.agreements), size = 0.2) +
+    geom_point(alpha = 0.5, position = position_dodge(dodge.width.agreements)) +
+    facet_wrap(~typeName, labeller = label_both) +
+    scale_x_discrete(name = 'Confidence Category', labels = c('Low', 'Med', 'High')) +
+    scale_color_discrete(name = 'Advice Type', labels = getAdviceTypeName(unique(tmp$adviceType))) +
+    scale_y_continuous(name = 'Advisor Agreement') +
+    labs(title = 'Observed agreement rate on initially correct trials') +
+    style
+}
+
+#' Trial count by confidence, advisor, and agreement
+#' @param trials trial set
+#' @param factorize whether to convert aggregate-by values to factors
+#' @param stripMissing whether to remove observations without complete data for each factor combination
+tmp.contingencyCounts <- function(trials, factorize = T, stripMissing = F) {
+  tmp <- aggregate(practice ~ 
+                     pid + confidenceCategory + adviceType + initialCorrect + advisorAgrees, 
+                   data = trials, FUN = length)
+  if(!factorize)
+    return(tmp)
+  for(n in names(tmp[-(length(tmp))]))
+    tmp[ ,n] <- factor(tmp[ ,n])
+  if(!stripMissing)
+    return(tmp)
+  # drop missing observations
+  miss <- aggregateMissing(tmp, 'pid', list('confidenceCategory', 'adviceType', 'initialCorrect', 'advisorAgrees'))
+  tmp <- tmp[!(tmp$pid %in% miss), ]
+}
+
+#' Graph of contingency counts
+#' @inheritDotParams tmp.contingencyCounts
+#' Additional arguments are passed to \code{tmp.contingencyCounts}
+gg.contingencyCounts <- function(trials, ...) {
+  tmp <- tmp.contingencyCounts(trials, ...)
+  ggplot(tmp, aes(y = practice, x = as.factor(confidenceCategory), 
+                       colour = as.factor(adviceType), shape = as.factor(advisorAgrees))) +
+    stat_summary(geom = 'point', size = 3, fun.y = mean, position = position_dodge(dodge.width.agreements)) +
+    stat_summary(geom = 'errorbar', fun.data = mean_cl_boot, position = position_dodge(dodge.width.agreements), size = 0.2) +
+    geom_point(alpha = 0.5, position = position_dodge(dodge.width.agreements)) +
+    facet_wrap(~initialCorrect, labeller = label_both) +
+    scale_x_discrete(name = 'Confidence Category', labels = c('Low', 'Med', 'High')) +
+    scale_color_discrete(name = 'Advice Type', labels = c('AiC', 'AiU')) +
+    scale_y_continuous(name = 'Trial Count') +
+    scale_shape_discrete(name = 'Advisor Agreement', labels = c('Disagree', 'Agree')) +
+    labs(title = 'Observed trial counts') +
+    style
+}
+
+
+# Hypothesis test functions -----------------------------------------------
+
+
+#' Aggregate influence by confidence, advisor, and agreement
+#' @param trials trial set
+#' @param factorize whether to convert aggregate-by values to factors
+#' @param stripMissing whether to remove observations without complete data for each factor combination
+tmp.influence <- function(trials, factorize = T, stripMissing = T) {
+  tmp <- aggregate(influence ~ adviceType + hasChoice + advisorAgrees + pid, data = trials, FUN = mean)
+  if(!factorize)
+    return(tmp)
+  for(n in names(tmp[-(length(tmp))]))
+    tmp[ ,n] <- factor(tmp[ ,n])
+  if(!stripMissing)
+    return(tmp)
+  # drop missing observations
+  miss <- aggregateMissing(tmp, 'pid', list('adviceType', 'hasChoice', 'advisorAgrees'))
+  tmp <- tmp[!(tmp$pid %in% miss), ]
+} 
+
+#' ANOVA on influence by the within-subjects factors
+aov.influence <- function(trials) {
+  tmp <- tmp.influence(trials)
+  aov(influence ~ adviceType * hasChoice * advisorAgrees + 
+        Error(pid / (adviceType + hasChoice + advisorAgrees)), 
+      data=tmp)
+}
+
+#' Print the means for the within-subjects factors
+#' @inheritDotParams tmp.influence
+#' @inheritParams tmp.influence
+#' 
+printMeans.influence <- function(trials, ...) {
+  tmp <- tmp.influence(trials, ...)
+  for(aT in unique(tmp$adviceType))
+    printMean(tmp$influence[tmp$adviceType==aT], 
+              paste0('Mean|', getAdviceTypeName(aT)))
+  printMean(tmp$influence[tmp$hasChoice==T], 'Mean|Choice')
+  printMean(tmp$influence[tmp$hasChoice==F], 'Mean|Forced')
+  printMean(tmp$influence[tmp$advisorAgrees==T], 'Mean|Agree')
+  printMean(tmp$influence[tmp$advisorAgrees==F], 'Mean|Disagree')
+  NULL
+}
+
+#' Graph of influence
+#' @inheritDotParams tmp.influence
+gg.influence <- function(trials, ...) {
+  tmp <- tmp.influence(trials, ...)
+  ggplot(tmp, aes(advisorAgrees, influence, color = adviceType, fill = adviceType)) +
+    geom_point(position = position_jitter(w=0.1, h=0), alpha = 0.5) +
+    stat_summary(geom = "errorbar", fun.data = "mean_cl_boot", width = 0.2) +
+    stat_summary(geom = "point", fun.y = "mean", shape = 23, size = 5) +
+    stat_summary(aes(group = adviceType), fun.y=mean, geom="line") + 
+    facet_grid(.~hasChoice, 
+               labeller = as_labeller(c('FALSE'='Forced trials','TRUE'='Choice trials'))) +
+    scale_color_discrete(name = 'Advisor type', labels = getAdviceTypeName(unique(tmp$adviceType))) +
+    scale_fill_discrete(name = 'Advisor type', labels = getAdviceTypeName(unique(tmp$adviceType))) +
+    scale_x_discrete(name = 'Judge-advisor agreement', labels = c('Disagree', 'Agree')) +
+    labs(title = "Advice Influence",
+         legend = NULL,
+         y = "Influence") +
+    style
+}
+
+# Data exploration --------------------------------------------------------
+
+#' Produce a dataframe of pick rate and forced trial agreement difference
+df.initialAgreement <- function(trials) {
+  tmp <- NULL
+  for(pid in unique(trials$pid)) {
+    ts <- trials[trials$pid == pid, ]
+    r <- NULL
+    for(i in length(unique(ts$block))/2) {
+      fBlock <- unique(ts$block)[2*(i-1)+1]
+      cBlock <- unique(ts$block)[2*(i-1)+2]
+      tsF <- ts[ts$block == fBlock, ]
+      tsC <- ts[ts$block == cBlock, ]
+      advisors <- unique(tsF$adviceType)
+      agrDiff <- mean(tsF$advisorAgrees[tsF$adviceType == advisors[1]]) - 
+        mean(tsF$advisorAgrees[tsF$adviceType == advisors[2]])
+      pickRate <- mean(tsC$adviceType == advisors[1])
+      # orient towards positive agreement difference
+      if(agrDiff < 0) {
+        agrDiff <- abs(agrDiff)
+        pickRate <- 1-pickRate
+        advisors <- rev(advisors)
+      } 
+      r <- rbind(r, data.frame(referenceAdvisor = advisors[1], 
+                               agreementDiff = agrDiff,
+                               pickRate))
+    }
+    r <- apply(r, 2, mean)
+    tmp <- rbind(tmp, data.frame(pid = factor(pid),
+                                       referenceAdvisor = r['referenceAdvisor'],
+                                       agreementDiff = r['agreementDiff'],
+                                       pickRate = r['pickRate']))
+  }
+
+  tmp$sameAdviceProfile <- round(tmp$referenceAdvisor) == tmp$referenceAdvisor
+  return(tmp)
+}
+
+#' Plot of forced trial agreement vs pickRate
+gg.initialAgreement <- function(trials) {
+  tmp <- df.initialAgreement(trials)
+  ggplot(tmp, aes(x = agreementDiff, y = pickRate, colour = pid, shape = sameAdviceProfile)) +
+    geom_hline(linetype = 'dashed', colour = 'black', yintercept = .5) +
+    geom_smooth(method = 'lm', colour = 'blue', fill = 'lightblue', alpha = .25) +
+    geom_point(alpha = 0.5) +
+    scale_shape_discrete(labels = c('Consistent', 'Inconsistent'), name = 'Most agreeing advisor') +
+    scale_x_continuous(expand = c(0.005,0)) +
+    labs(title = 'Initial agreement effects',
+         y = 'P(More agreeing advisor chosen)',
+         x = 'Mean agreement difference on Forced trials') +
+    style.long
+}
--- analysis/miscFunctions.R
+++ analysis/miscFunctions.R
@@ -6,8 +6,9 @@
 #' Does not respect scientific notation options
 #' @param proportion number to convert to string
 #' @param precision decimal places to preserve
+#' @param truncateZeros whether to strip trailing 0s
 #' @return \{code}proportion stripped of leading 0s and rounded to \{code}precision decimal places
-prop2str <- function(proportion, precision = 2) {
+prop2str <- function(proportion, precision = 2, truncateZeros = F) {
   if(length(proportion) > 1)
     return(sapply(proportion, prop2str))
   if(is.nan(proportion))
@@ -17,6 +18,35 @@ prop2str <- function(proportion, precision = 2) {
     x <- sub('^-?0\\.', ifelse(proportion < 0, '-.', '.'), as.character(proportion))
   else 
     x <- as.character(proportion)
+  if(truncateZeros)
+    return(x)
+  dot <- regexpr('.', x, fixed = T)
+  if(dot == -1) {
+    x <- paste0(x,'.')
+    dot <- regexpr('.', x, fixed = T)
+  }
+  right <- substr(x, dot, dot+precision) # portion of x after 0
+  right <- paste0(right, strrep('0',precision-nchar(right)+1))
+  x <- paste0(substr(x, 1, dot-1), right)
+  return(x)
+}
+
+#' Format number to be a string beginning with a decimal point.
+#' Does not respect scientific notation options
+#' @param num number to convert to string
+#' @param precision decimal places to preserve
+#' @param truncateZeros whether to strip trailing 0s
+#' @return \{code}num stripped of leading 0s and rounded to \{code}precision decimal places
+num2str <- function(num, precision = 2, truncateZeros = F) {
+  if(length(num) > 1)
+    return(sapply(num, num2str))
+  if(is.nan(num))
+    return(as.character(num))
+  num <- round(num, precision)
+  # string manipulation to pad 0s
+  x <- as.character(num)
+  if(truncateZeros)
+    return(x)
   dot <- regexpr('.', x, fixed = T)
   if(dot == -1) {
     x <- paste0(x,'.')
@@ -53,17 +83,19 @@ prettyPrint <- function(results, d = NULL, doPrint = T) {
 #' @param conf.int width of the confidence intervals
 #' @param na.rm whether NA values are removed before averaging
 #' @param decimals decimal places to round to
+#' @param isProportion whether to print the values as proportions (strip leading 0)
 #' @return string representation of the mean, CIs, and range of the \{code}vector
-printMean <- function(vector, label = 'Mean', doPrint = T, conf.int = .95, na.rm = F, decimals = 2) {
+printMean <- function(vector, label = 'Mean', doPrint = T, conf.int = .95, na.rm = F, decimals = 2, isProportion = F) {
+  precisionFun <- ifelse(isProportion, prop2str, num2str)
   mu <- mean(vector, na.rm = na.rm)
   s <- sd(vector, na.rm = na.rm)
   n <- length(vector)
   error <- qnorm(1-(1-conf.int)/2)*s/sqrt(n) # 95% confidence interval width
   ci.low <- mu - error
   ci.high <- mu + error
-  r <- round(range(vector, na.rm = na.rm), decimals)
-  out <- paste0(label,'=', round(mu,decimals), ' [', round(conf.int,decimals)*100, '%CI: ',
-               round(ci.low,decimals), ', ', round(ci.high,decimals),'] [Range: ',
+  r <- precisionFun(range(vector, na.rm = na.rm), decimals)
+  out <- paste0(label,'=', precisionFun(mu,decimals), ' [', precisionFun(conf.int*100,decimals,F), '%CI: ',
+                precisionFun(ci.low,decimals), ', ', precisionFun(ci.high,decimals),'] [Range: ',
                r[[1]], ', ', r[[2]], ']')
   if(doPrint)
     print(out)
@@ -101,11 +133,33 @@ gg.font.large <- 24
 style <- theme_light() +
   theme(panel.grid.minor = element_blank(),
         panel.grid.major.x = element_blank(),
-        text = element_text(size = gg.font.small),
-        plot.title = element_text(size = gg.font.med),
+        text = element_text(size = gg.font.med),
+        plot.title = element_text(size = gg.font.large, margin=margin(0,0,gg.font.large,0)),
         legend.position = 'top')
 
 style.long <- style + theme(legend.position = 'none')
 
 # Utility functions -------------------------------------------------------
 
+#' Find missing values in an aggreagated data frame
+#' @param df data frame to investigate
+#' @param id.colName column of ids to check for missing rows
+#' @param factor.colNames list of factor columns which must be present for all
+#'   ids at all factor level combinations
+#'
+#' @return vector of id.colName values which do not have values at all levels
+#'   for all factors. 
+#'  
+#' WARNING: Just tests numbers, not actual values!
+aggregateMissing <- function(df, id.colName, factor.colNames) {
+  out <- NULL
+  lvls <- 1
+  for(x in factor.colNames) {
+    lvls <- lvls * length(levels(df[ , x]))
+  }
+  for(id in unique(df[ , id.colName])) {
+    if(sum(df[ , id.colName] == id) < lvls)
+      out <- c(out, id)
+  }
+  return(out)
+}
