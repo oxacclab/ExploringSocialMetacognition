cd519f388352702374ae38eb3552de3746ff3247
mjaquiery <m.jaquiery@sussex.ac.uk>
2019-03-15 11:26:55 +0000

    android test

---
+++ src/modules/Advisor.js
@@ -0,0 +1,14 @@
+/**
+ * Advisor
+ * Matt Jaquiery, March 2019
+ *
+ * Javascript library for running social metacognition studies.
+ */
+
+"use strict";
+
+class Advisor {
+
+}
+
+export default {Advisor};
