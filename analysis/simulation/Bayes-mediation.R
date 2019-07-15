2e5cdf3450a06b2b198a8343b0044ef6f7b58e9a
mjaquiery <m.jaquiery@sussex.ac.uk>
2019-07-15 10:38:27 +0100

    Update Bayes-mediation.R

--- analysis/simulation/Bayes-mediation.R
+++ analysis/simulation/Bayes-mediation.R
@@ -14,29 +14,73 @@
 # dependencies ------------------------------------------------------------
 library(BayesMed)
 library(tibble)
+library(ggplot2)
+library(parallel)
 
-# make data ---------------------------------------------------------------
 
-effectSizes <- tibble(a = c(.1, .3, .7),
-                      b = c(.1, .3, .7),
-                      c = c(.1, .3, .7))
+# cluster variables -------------------------------------------------------
 
-n <- 500
+effectSizes <- list()
+es <- c(.1, .3, .7)
+ns <- c(20, 50, 100, 500)
+for (a in es) {
+  for (b in es) {
+    for (n in ns) {
+      effectSizes[[length(effectSizes) + 1]] <- tibble(a, b, n)
+    }
+  }
+}
 
-simData <- tibble(p = rep(seq(n), each = 2), # participant numbers
-                  sigma = rep(abs(rnorm(n)), each = 2), # each participant gets idiosynratic noise
-                  Group = rep(seq(2), times = n),
-                  Q = rep(rnorm(n), each = 2) + # intercept
-                    effectSizes$a[2] * Group + # group effect
-                    rnorm(n * 2, sigma), # error
-                  WoA = rep(rnorm(n), each = 2) + 
-                    effectSizes$b[2] * Q +
-                    effectSizes$c[2] * Group +
-                    rnorm(n * 2, sigma))
 
-# run bayes ---------------------------------------------------------------
+# cluster function --------------------------------------------------------
 
+runSim <- function(props) {
+  d <- tibble::tibble(p = rep(seq(props$n), each = 2), # participant numbers
+                      sigma = rep(abs(rnorm(props$n)), each = 2), # each participant gets idiosynratic noise
+                      Group = rep(seq(2), times = props$n) - 1,
+                      Q = rep(rnorm(props$n), each = 2) + # intercept
+                        props$a * Group + # group effect
+                        rnorm(props$n * 2, sd = sigma), # error
+                      WoA = rep(rnorm(props$n), each = 2) + 
+                        props$b * Q +
+                        rnorm(props$n * 2, sd = sigma))
+  
+  # mediation analysis
+  result <- BayesMed::jzs_med(independent = d$Group, 
+                              dependent = d$WoA, 
+                              mediator = d$Q)
+  
+  list(props = props, data = d, result = result)
+}
+
+
+# run in parallel ---------------------------------------------------------
+
+nCores <- detectCores() - 4
+cl <- makeCluster(nCores)
+
+startTime <- Sys.time()
+output <- clusterApply(cl, effectSizes, fun = runSim)
+print(paste0("Time elapsed: ", Sys.time() - startTime))
 
 # show results ------------------------------------------------------------
 
+grid <- NULL
+
+for (x in output) {
+  grid <- rbind(grid, tibble(a = x$props$a, b = x$props$b, n = x$props$n,
+                             bf = x$result$main_result$BF[4]))
+}
+
+# heatmap 
+for (n in unique(grid$n)) {
+  print(
+    ggplot(grid[grid$n == n, ], aes(a, b, fill = log(bf))) +
+      geom_tile(colour = "white") +
+      scale_fill_gradient2() +
+      labs(x = "Group -> Q", y = "Q -> WoA",
+           title = "BF for mediation of Group -> WoA relationship",
+           subtitle = paste("n =", n))
+  )
+}
 
