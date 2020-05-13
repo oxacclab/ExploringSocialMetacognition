dc088bb3e2b089b63e748858c2e21364fec3c2d7
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2020-05-13 09:36:34 +0100

    debugging save script on server

--- saveSerial.php
+++ saveSerial.php
@@ -44,7 +44,7 @@ registration), or the following properties:
 
 //phpinfo();
 error_reporting(0);
-//ini_set("display_errors", true);
+ini_set("display_errors", true);
 ini_set("auto_detect_line_endings", true);
 $log = "";
 
